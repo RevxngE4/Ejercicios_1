19da1c4205e4b00aab7c52e67460c29fec281177
RevxngE4 <131587201+RevxngE4@users.noreply.github.com>
2023-06-14 18:12:17 +0300

    Add files via upload

---
+++ Tipos_de_datos/Ejercicios_tema1.java
@@ -0,0 +1,18 @@
+package Tipos_de_datos;
+
+public class Ejercicios_tema1 {
+    public static void main(String[] args) {
+        int numero1 = 45;
+        long numero2 = 45000;
+        double numero3 = 4.5;
+        boolean value = true;
+        String nombre = "Camilo";
+        System.out.println(numero1);
+        System.out.println(numero2);
+        System.out.println(numero3);
+        System.out.println(value);
+        System.out.println(nombre);
+        
+    }
+
+}
